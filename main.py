c2a2c749e4c2f0227ee0f5e069065edfb83946e3
ColomboRiccardo <colombo.riccardo@protonmail.com>
2026-01-28 11:08:19 +0100

    finished project

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from stats import get_num_words, get_letter_count
+from stats import get_num_words, get_letter_count, sort_letter_dict
+import sys
 
 def get_book_text(filename):
 	with open(filename) as f:
 		file_contents = f.read()
-		print(f"Found {get_num_words(file_contents)} total words")
-		print(f"Found {get_letter_count(file_contents)} total letters")
+		# print(f"Found {get_num_words(file_contents)} total words")
+		# print(f"Found {get_letter_count(file_contents)} total letters")
+		letters_dict = get_letter_count(file_contents)
+		sorted_letters_dict = sort_letter_dict(letters_dict)
+
+		result_to_print = ""
+
+		for letter in sorted_letters_dict:
+			result_to_print += f"{letter["char"]}: {letter["num"]}\n"
+
+		print(f"""
+		============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {get_num_words(file_contents)} total words
+--------- Character Count -------
+{result_to_print}
+============= END ===============
+		""")
 		
 
 def main():
-	get_book_text("./books/frankenstein.txt")
+	if len(sys.argv) >= 2:
+		get_book_text(sys.argv[1])
+	else:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
 
 main()
--- stats.py
+++ stats.py
@@ -1,13 +1,24 @@
+
 def get_num_words(content):
 	return len(content.split())
 
 def get_letter_count(content):
 	letter_dict = {}
 	for letter in content:
-		if letter == " ":
-			pass
+		if not letter.isalpha():
+			continue
 		if letter.lower() in letter_dict:
 			letter_dict[letter.lower()] += 1
 		else:
 			letter_dict[letter.lower()] = 1
 	return letter_dict
+
+def sort_letter_dict(letter_dict):
+	result_list = []
+	for char, num in letter_dict.items():
+		result_list.append({"char": char, "num": num})
+	result_list.sort(reverse=True, key=sort_on)
+	return result_list
+
+def sort_on(items):
+	return items["num"]
